4dc5d14878605814258beca7a37c5fa0a7627b00
benwasden <was24001@byui.edu>
2024-11-20 18:50:29 -0800

    Adding weather to home page

--- scripts/home.js
+++ scripts/home.js
@@ -168,5 +168,36 @@ function displayCourseInfo(filteredClasses) {
 
     document.querySelector('#credits').innerHTML = `Total credits: ${credits}`;
     document.querySelector('#completed-credits').innerHTML = `Completed: ${completedCredits}`;
-
 }
+
+const currentTemp = document.querySelector("#current-temp");
+const image = document.querySelector("#weather-icon");
+const weatherConditions = document.querySelector("#conditions");
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=37.80&lon=-122.27&appid=499d17cb2fcb9bce311b043aee09342e&units=imperial'
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp} F`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    let desc = data.weather[0].description;
+    image.setAttribute('src', iconsrc);
+    image.setAttribute('alt', data.weather[0].description);
+    weatherConditions.textContent = `${desc.charAt(0).toUpperCase() + desc.slice(1)}`;
+}
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+apiFetch();
