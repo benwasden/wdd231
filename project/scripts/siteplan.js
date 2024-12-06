96a2efbb23b666a12c2868ed13da592153a98480
benwasden <was24001@byui.edu>
2024-12-05 20:02:04 -0800

    Site Plan

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,7 @@
+// Year for copyright year
+const currentYear = document.getElementById('currentyear');
+currentYear.textContent = new Date().getFullYear();
+
+// Date/Time for last modification for website
+const lastModified = document.getElementById('lastModified');
+lastModified.textContent = (document.lastModified);
