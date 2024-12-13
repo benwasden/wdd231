9aa0763d5feb4bdb27039d31a0466bf92df0ad3c
benwasden <was24001@byui.edu>
2024-12-13 13:19:14 -0800

    Page Start

---
+++ project/scripts/home.js
@@ -0,0 +1,17 @@
+// Year for copyright year
+const currentYear = document.getElementById('currentyear');
+currentYear.textContent = new Date().getFullYear();
+
+// Date/Time for last modification for website
+const lastModified = document.getElementById('lastModified');
+lastModified.textContent = (document.lastModified);
+
+// Variables for menu button in mobile view
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('#animateme');
+
+// Event listener for menu button
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
