9bbfe163b03eb7e8f624d91742f0a2555537a091
benwasden <was24001@byui.edu>
2024-12-14 10:08:34 -0800

    Adding JS to form

--- project/scripts/build.js
+++ project/scripts/build.js
@@ -15,3 +15,57 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+
+const currentUrl = window.location.href;
+const everything = currentUrl.split('?');
+
+let formData = everything[1].split('&');
+
+function show(info) {
+    formData.forEach((element) => {
+        if (element.startsWith(info)) {
+            result = element.split('=')[1].replace("%40", "@").replace("%2B", "+").replaceAll("+", " ").replaceAll("%2F", "/").replace("%2C"," ").replaceAll("%3A",":");
+        }
+    });
+    return result;
+};
+
+const formCount = document.getElementById('formCount');
+
+// if (localStorage.getItem('numOfForms') == null) {
+//     formCount.innerHTML = "Thanks for submitting a form! This is your first form with us."
+//     localStorage.setItem('numOfForms', show('count'))
+//     console.log(numOfForms)
+// }
+// else {
+//     console.log(localStorage.getItem('numOfForms'));
+//     const currentFormCount = Number(localStorage.getItem('numOfForms')) + Number(show('count'));
+//     formCount.innerHTML = `Thanks for submitting the form! You've submitted ${currentFormCount} forms with us.`
+//     localStorage.setItem('numOfForms', currentFormCount);
+// }
+
+
+
+const showInfo = document.querySelector('#results');
+showInfo.innerHTML = `
+<p><strong>Name:</strong> ${show('fname')} ${show('lname')}
+<p><strong>Email:</strong> <a href='mailto: ${show('email')}'>${show('email')}</a></p>
+<p><strong>Use case:</strong> ${show('usecase')}
+<p><strong>Budget:</strong> $${show('budget')}
+<p><strong>Date Submitted: </strong> ${show('fecha')}`
+
+
+const builds = "data/builds.json";
+
+async function getBuilds() {
+    const response = await fetch(builds);
+    const data = await response.json();
+    
+    const gaming = data.gaming;
+    console.log(gaming);
+
+    show(currentUrl);
+
+}
+
+getBuilds();
--- project/scripts/parts.js
+++ project/scripts/parts.js
@@ -22,3 +22,19 @@ const dateAndTime = document.getElementById('submit');
 dateAndTime.addEventListener('click', () => {
     document.getElementById('date-and-time').value = date.toLocaleString();
 })
+
+
+// Adding a submission count feature for the submitted form. Need to use event listener
+// to not count refreshing the form page as a submission.
+const submittedForms = document.getElementById('submission-count')
+
+document.getElementById('submit').addEventListener('click', () => {
+    if (localStorage.getItem('numOfForms') == null) {
+        submittedForms.value = 1;
+        localStorage.setItem('numOfForms', submittedForms.value);
+    }
+    else {
+        submittedForms.value = Number(localStorage.getItem('numOfForms')) + 1;
+        localStorage.setItem('numOfForms', submittedForms.value);
+    }
+})
